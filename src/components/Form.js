0d3c1e7ca0e6c53d44aaf1b5f3f2de91cbf763e8
Darien <darienweise@gmail.com>
2020-10-03 13:39:15 -0400

    form img compDidMount added, some styles added for category slide

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import categoryList from '../categoryList';
+
 
 
 class Form extends Component{
+   constructor(){
+      super();
+      this.state = {
+         category: {},
+      }
+   }
+   componentDidMount(){
+      const slideType = this.props.match.params.categoryName;
+      console.log(categoryList)
+      categoryList.forEach( category =>{
+         if (category.name === slideType){
+            console.log(category)
+            this.setState({
+               category: category
+            })
+         }
+      })
+   }
    render(){
+      const {name, path} = this.state.category;
       return(
-         <h1>Form Component</h1>
+         <div>
+            <div className="slidePreview">
+               <h2>{name}</h2>
+               <div className="slideImgContainer">
+                  <img src={path}/>
+               </div>
+            </div>
+         </div>
       )
    }
 }
