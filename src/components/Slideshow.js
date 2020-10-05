5a0222246fba47ee3bd6723954f720c8d877a377
Jason <youchia1991@gmail.com>
2020-10-05 17:25:14 -0400

    indentation

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -44,14 +44,15 @@ class Slideshow extends Component{
       return(
          <div className = "slideshowContainer">
             <button className = "slideshowBtnLeft" disabled={this.state.currentSlide  === 0}>
-               <i className="fa fa-chevron-left" onClick = {this.prevNextHandler} name = "previous"  ></i>
+               <i className="fa fa-chevron-left" onClick ={this.prevNextHandler} name ="previous"></i>
                {/* &laquo; */}
             </button>
             <div className = "slideImg">
                {this.props.images.length > 1 ? 
                   (  this.state.currentSlide < this.props.images.length ?
                      <div className="imageContainer"> 
-                        <img src = {this.props.images[this.state.currentSlide].url} alt = {this.props.images[this.state.currentSlide].alt}/>
+                        <img src={this.props.images[this.state.currentSlide].url} 
+                        alt={this.props.images[this.state.currentSlide].alt}/>
                         <div className="quoteContainer">
                            <p>{this.props.quotes[this.state.currentSlide]}</p>  
                         </div> 
@@ -62,7 +63,7 @@ class Slideshow extends Component{
                }      
             </div>
             <button className = "slideshowBtnRight" disabled={this.state.currentSlide === this.props.images.length}>
-               <i className="fa fa-chevron-right" onClick = {this.prevNextHandler} name = "next"></i>
+               <i className="fa fa-chevron-right" onClick ={this.prevNextHandler} name="next"></i>
                {/* &raquo; */}
             </button>
          </div>      
