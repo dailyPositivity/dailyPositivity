b0e770776af38c3c4732d78f61a150dce691c036
Neha <nehasuthr@gmail.com>
2020-10-05 14:50:03 -0400

    Added Slideshow Component logic

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,36 +1,67 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import categoryList from '../categoryList';
-import InputQuote from './InputQuote';
-import StartSlideshowButton from './StartSlideshowButton';
 
 class Slideshow extends Component{
-   getCategoryImagePath(){  
-      const filteredCategory = categoryList.filter((category) => {
-            return(category.name === this.props.match.params.categoryName);
+   constructor(props){
+      super(props);
+      this.state = {
+         currentSlide:0,
+      }
+   }
+   prevNextHandler = (event) => {
+      console.log(event.target.name);
+      if(event.target.name === "previous"){
+         if(this.state.currentSlide > 0){
+            this.setState({
+               currentSlide:this.state.currentSlide-1
+            })
+         } 
+      }else if(event.target.name === "next"){
+         if(this.state.currentSlide < this.props.images.length){
+            const currentState = this.state.currentSlide;
+            this.setState({
+               currentSlide:currentState+1
+            })
+         }
+      }
+   }
+
+   autoPlay = () => {
+      if(this.state.currentSlide < this.props.images.length){
+         const currentState = this.state.currentSlide;
+         this.setState({
+            currentSlide:currentState+1
          })
-      return filteredCategory[0];
+      }
+   }
+
+   componentDidMount(){
+      setInterval(this.autoPlay,3000);
    }
 
    render(){
-      //filter categoryList array as per passed in category by Category Component
-      const {name,path,description} = {...this.getCategoryImagePath()};
-
       return(
-               <div className = "slideshowContainer">
-                     <button className = "slideshowBtnLeft">
-                        <i className="fa fa-chevron-left"></i>
-                     </button>
-                   <div className = "slideImg">
-                     <img src = {this.props.images[0].url} alt = {description}/>
-                     <h3>{name}</h3>
-                   </div>
-                     <button className = "slideshowBtnRight">
-                        <i className="fa fa-chevron-right"></i>
-                     </button>
-               </div>
+
+         <div className = "slideshowContainer">
+            <button className = "slideshowBtnLeft" disabled={this.state.currentSlide  === 0}>
+               <i className="fa fa-chevron-left" onClick = {this.prevNextHandler} name = "previous" ></i>
+               {/* &laquo; */}
+            </button>
+            <div className = "slideImg">
+            {  this.props.images.length> 1 ? 
+               (  this.state.currentSlide < this.props.images.length ? 
+                  <img src = {this.props.images[this.state.currentSlide].url} alt = {this.props.images[this.state.currentSlide].alt}/>                   
+                  : <h2 className="endingMessage">Thank you for watching</h2>  
+                  )             
+               : <h2 className="endingMessage">Category not selected.Select the category first.</h2>  
+            }             
+            </div>
+            <button className = "slideshowBtnRight" disabled={this.state.currentSlide === this.props.images.length}>
+               <i className="fa fa-chevron-right" onClick = {this.prevNextHandler} name = "next"></i>
+               {/* &raquo; */}
+            </button>
+         </div>      
       )
    }
 }
 
-export default withRouter(Slideshow);
+export default Slideshow;
