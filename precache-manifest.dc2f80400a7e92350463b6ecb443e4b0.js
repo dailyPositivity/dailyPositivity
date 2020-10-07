2ea4de8c8a00607796f5acadf337d4b41ed32e6a
Evan Wallace <evan.wallace@mail.utoronto.ca>
2020-10-07 16:08:04 -0400

    Updates

---
+++ precache-manifest.dc2f80400a7e92350463b6ecb443e4b0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c29e4e5aa6044bcfe392b282a52bec",
+    "url": "/dailyPositivity/index.html"
+  },
+  {
+    "revision": "b392d5cb692e3f505053",
+    "url": "/dailyPositivity/static/css/main.9ceb6cb5.chunk.css"
+  },
+  {
+    "revision": "87049f94f85b6a32e9d1",
+    "url": "/dailyPositivity/static/js/2.8ddd9479.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dailyPositivity/static/js/2.8ddd9479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b392d5cb692e3f505053",
+    "url": "/dailyPositivity/static/js/main.5fe5dfec.chunk.js"
+  },
+  {
+    "revision": "75ac16c64fb6e57cc6a7",
+    "url": "/dailyPositivity/static/js/runtime-main.0a52199f.js"
+  },
+  {
+    "revision": "4e66105da92fb08636c1fb6373f4828f",
+    "url": "/dailyPositivity/static/media/category-beach.4e66105d.jpg"
+  },
+  {
+    "revision": "a1491f68b43fe7cddfe40d11eb27ef0e",
+    "url": "/dailyPositivity/static/media/category-desert.a1491f68.jpg"
+  },
+  {
+    "revision": "daa6dbd406adbf2698844f73a8e2c82f",
+    "url": "/dailyPositivity/static/media/category-forest.daa6dbd4.jpg"
+  },
+  {
+    "revision": "d8750fd5f0eaad1dd33ae7fc70b65aa7",
+    "url": "/dailyPositivity/static/media/category-sky.d8750fd5.jpg"
+  },
+  {
+    "revision": "9cf94e4dfb2acee3c3948e2d7c270b61",
+    "url": "/dailyPositivity/static/media/category-space.9cf94e4d.jpg"
+  },
+  {
+    "revision": "5b4222fdc1503cd33c68fe4d06e3b935",
+    "url": "/dailyPositivity/static/media/category-twilight.5b4222fd.jpg"
+  }
+]);
