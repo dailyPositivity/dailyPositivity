437954bba343730412f4708b4039fc4b5ec6ca27
Evan Wallace <evan.wallace@mail.utoronto.ca>
2020-10-07 20:59:44 -0400

    add alt text to Header and remove comments

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,7 @@ class Header extends Component{
             <div className = "wrapper">
                 <div className = "headerContainer">
                     <Link to = "/">
-                        <img className="logo" src={require("../Assets/misty-day.png")}/>
-                        {/* OTHER IMAGES
-                           https://www.flaticon.com/svg/static/icons/svg/1295/1295535.svg
-                           https://www.flaticon.com/svg/static/icons/svg/3159/3159094.svg
-                           https://www.flaticon.com/svg/static/icons/svg/2714/2714077.svg */}
+                        <img className="logo" src={require("../Assets/misty-day.png")} alt="Outline of a red sun rising above 3 horizonal red lines." />
                     </Link>
                 </div>
             </div>
