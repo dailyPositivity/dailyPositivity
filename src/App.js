a09ccf59b45768801363c5a0276a242397615f87
Evan Wallace <evan.wallace@mail.utoronto.ca>
2020-10-07 22:23:02 -0400

    switch back to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 import axios from 'axios';
-import { BrowserRouter as Router, Route  } from 'react-router-dom';
+import { HashRouter as Router, Route  } from 'react-router-dom';
 
 // Components
 import Category from './components/Category';
